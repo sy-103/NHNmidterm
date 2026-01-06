c93425102f2ef54e3eb4df3649e9d13380af02ce
sy-103 <tpdbs3616@naver.com>
2026-01-06 15:12:05 +0900

    first commit

--- src/main/java/com/nhn/academy/todo/service/TodoService.java
+++ src/main/java/com/nhn/academy/todo/service/TodoService.java
@@ -3,7 +3,6 @@ package com.nhn.academy.todo.service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.nhn.academy.todo.model.Todo;
